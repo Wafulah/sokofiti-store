e0e05013cce28214bdc92dcc95a5f176a12f86a3
Wafulah <wafulahvictor@gmail.com>
2023-10-20 18:10:35 +0300

    mpesa

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import axios from "axios";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 
 import Button from "@/components/ui/button";
 import Currency from "@/components/ui/currency";
+import Modal from "@/components/ui/modal";
 import useCart from "@/hooks/use-cart";
 import { toast } from "react-hot-toast";
 
@@ -13,48 +14,82 @@ const Summary = () => {
   const searchParams = useSearchParams();
   const items = useCart((state) => state.items);
   const removeAll = useCart((state) => state.removeAll);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const onCheckout = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   useEffect(() => {
-    if (searchParams.get('success')) {
-      toast.success('Payment completed.');
+    if (searchParams.get("success")) {
+      toast.success("Payment completed.");
       removeAll();
     }
 
-    if (searchParams.get('canceled')) {
-      toast.error('Something went wrong.');
+    if (searchParams.get("canceled")) {
+      toast.error("Something went wrong.");
     }
   }, [searchParams, removeAll]);
 
   const totalPrice = items.reduce((total, item) => {
-    return total + Number(item.price)
+    return total + Number(item.price);
   }, 0);
 
-  const onCheckout = async () => {
-    const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/checkout`, {
-      productIds: items.map((item) => item.id)
-    });
+
+  const onStripe = async () => {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/checkout`,
+      {
+        productIds: items.map((item) => item.id),
+      }
+    );
 
     window.location = response.data.url;
-  }
+  };
 
-  return ( 
-    <div
-      className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8"
-    >
-      <h2 className="text-lg font-medium text-gray-900">
-        Order summary
-      </h2>
+  const onMpesa = async () => {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/mpesa_pay`,
+      {
+        productIds: items.map((item) => item.id),
+      }
+    );
+
+    window.location = response.data.url;
+  };
+
+  return (
+    <div className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
+      {isModalOpen && (
+       <div className="p-4"><Modal open={isModalOpen} onClose={closeModal}>
+          <div className="pt-6 space-x-2 flex  w-1/2 mx-auto">
+            <Button className="rounded-lg" onClick={onStripe}>Credit Card</Button>
+            <Button className="bg-green-500 rounded-lg" onClick={onMpesa}>Mpesa</Button>
+          </div>
+
+        </Modal>
+        </div>
+      )}
+      <h2 className="text-lg font-medium text-gray-900">Order summary</h2>
       <div className="mt-6 space-y-4">
         <div className="flex items-center justify-between border-t border-gray-200 pt-4">
           <div className="text-base font-medium text-gray-900">Order total</div>
-         <Currency value={totalPrice} />
+          <Currency value={totalPrice} />
         </div>
       </div>
-      <Button onClick={onCheckout} disabled={items.length === 0} className="w-full mt-6">
+      <Button
+        onClick={onCheckout}
+        disabled={items.length === 0}
+        className="w-full mt-6"
+      >
         Checkout
       </Button>
     </div>
   );
-}
- 
+};
+
 export default Summary;
