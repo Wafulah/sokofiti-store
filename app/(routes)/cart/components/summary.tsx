4cf342cf9ea546c1536e17f5121de99ba7078ac2
Wafulah <wafulahvictor@gmail.com>
2023-10-22 02:19:13 +0300

    modals

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -9,12 +9,14 @@ import Currency from "@/components/ui/currency";
 import Modal from "@/components/ui/modal";
 import useCart from "@/hooks/use-cart";
 import { toast } from "react-hot-toast";
+import { StoreModal } from "@/components/modals/store-modal";
 
 const Summary = () => {
   const searchParams = useSearchParams();
   const items = useCart((state) => state.items);
   const removeAll = useCart((state) => state.removeAll);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [details, setDetails] = useState(false);
 
   const onCheckout = () => {
     setIsModalOpen(true);
@@ -39,7 +41,6 @@ const Summary = () => {
     return total + Number(item.price);
   }, 0);
 
-
   const onStripe = async () => {
     const response = await axios.post(
       `${process.env.NEXT_PUBLIC_API_URL}/checkout`,
@@ -52,29 +53,30 @@ const Summary = () => {
   };
 
   const onMpesa = async () => {
-    const response = await axios.post(
-      `${process.env.NEXT_PUBLIC_API_URL}/mpesa_pay`,
-      {
-        productIds: items.map((item) => item.id),
-      }
-    );
-
-    window.location = response.data.url;
+    setIsModalOpen(false);
+    setDetails(true);
+    
   };
 
   return (
     <div className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
       {isModalOpen && (
-       <div className="p-4"><Modal open={isModalOpen} onClose={closeModal}>
-          <div className="pt-6 space-x-2 flex  w-1/2 mx-auto">
-            <Button className="rounded-lg" onClick={onStripe}>Credit Card</Button>
-            <Button className="bg-green-500 rounded-lg" onClick={onMpesa}>Mpesa</Button>
-          </div>
-
-        </Modal>
+        <div className="p-4">
+          <Modal open={isModalOpen} onClose={closeModal}>
+            <div className="pt-6 space-x-2 flex  w-1/2 mx-auto">
+              <Button className="rounded-lg" onClick={onStripe}>
+                Credit Card
+              </Button>
+              <Button className="bg-green-500 rounded-lg" onClick={onMpesa}>
+                Mpesa
+              </Button>
+            </div>
+          </Modal>
         </div>
       )}
+      {details && <StoreModal />}
       <h2 className="text-lg font-medium text-gray-900">Order summary</h2>
+      
       <div className="mt-6 space-y-4">
         <div className="flex items-center justify-between border-t border-gray-200 pt-4">
           <div className="text-base font-medium text-gray-900">Order total</div>
---
+++ components/modals/store-modal.tsx
@@ -0,0 +1,139 @@
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import useCart from "@/hooks/use-cart";
+
+import  Modal  from "@/components/ui/modal";
+import { Input } from "@/components/ui/input";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import Button from "@/components/ui/button";
+
+const formSchema = z.object({
+  name: z.string().min(1),
+  phoneNo: z
+    .string()
+    .refine((phoneNo) => /^\d{12}$/.test(phoneNo), {
+      message: "Phone number must start with 254",
+    }),
+});
+
+export const StoreModal = () => {
+  const storeModal = useStoreModal();
+  const router = useRouter();
+  const items = useCart((state) => state.items);
+  const [isModalOpen, setIsModalOpen] = useState(true);
+  const [loading, setLoading] = useState(false);
+  
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      phoneNo: "254712345678", // Initialize it as an integer with exactly 9 digits
+    },
+  });
+  
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setLoading(true);
+
+      // You can access form values like this:
+      const { name, phoneNo } = values;
+      console.log(name,phoneNo);
+      // Send the data to your API endpoint
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/mpesa_pay`,
+        {
+          name,
+          phoneNo,
+          productIds: items.map((item) => item.id),
+        }
+      );
+
+      // Handle the response or redirection here
+      window.location = response.data.url;
+    } catch (error) {
+      console.error("Error:", error);
+      toast.error("An error occurred.");
+    } finally {
+      setLoading(false);
+    }
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <Modal open={isModalOpen} onClose={closeModal}>
+      
+       <div>
+        <div className="space-y-4 py-2 pb-4">
+          <div className="space-y-2">
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)}>
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>County</FormLabel>
+                      <FormControl>
+                        <Input
+                          disabled={loading}
+                          placeholder="County"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {/* Add the phoneNo field similar to the name field */}
+                 <FormField
+                  control={form.control}
+                  name="phoneNo"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Phone Number</FormLabel>
+                      <FormControl>
+                        <Input
+                          disabled={loading}
+                          placeholder="Phone Number"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                  <Button disabled={loading} onClick={storeModal.onClose}>
+                    Cancel
+                  </Button>
+                  <Button disabled={loading} type="submit">
+                    Continue
+                  </Button>
+                </div>
+              </form>
+            </Form>
+          </div>
+        </div>
+      </div>  
+    </Modal>
+  );
+};
