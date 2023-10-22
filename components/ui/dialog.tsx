6a69c87abfa6ac56723a3c54c684f57e8129cf55
Wafulah <wafulahvictor@gmail.com>
2023-10-22 04:42:11 +0300

    CustomDialogPortalProps

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -9,12 +9,15 @@ import { cn } from "@/lib/utils"
 const Dialog = DialogPrimitive.Root
 
 const DialogTrigger = DialogPrimitive.Trigger
+interface CustomDialogPortalProps extends DialogPrimitive.DialogPortalProps {
+  className?: string;
+}
 
 const DialogPortal = ({
   className,
   children,
   ...props
-}: DialogPrimitive.DialogPortalProps) => (
+}: CustomDialogPortalProps) => (
   <DialogPrimitive.Portal className={cn(className)} {...props}>
     <div className="fixed inset-0 z-50 flex items-start justify-center sm:items-center">
       {children}
