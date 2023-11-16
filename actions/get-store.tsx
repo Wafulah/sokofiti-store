020f6fce45f1252e6f5b8f78eaaeddffb04a5024
Wafulah <wafulahvictor@gmail.com>
2023-11-17 01:05:23 +0300

    res

--- actions/get-store.tsx
+++ actions/get-store.tsx
@@ -1,6 +1,6 @@
 import { Store } from "@/types";
 
-const URL=`${process.env.NEXT_PUBLIC_API_URL}/stores`;
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/stores`;
 
 const getStore = async (id: string): Promise<Store> => {
   const res = await fetch(`${URL}/${id}`);
@@ -9,7 +9,7 @@ const getStore = async (id: string): Promise<Store> => {
     throw new Error(`HTTP error! Status: ${res.status}`);
   }
 
-  const data: Store[] = await res.json();
+  const data: Store = await res.json();
   console.log(data);
   return data;
 };
