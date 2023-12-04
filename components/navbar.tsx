d31349c24a1110a1cd90a5a49eed90939679fb38
Wafulah <wafulahvictor@gmail.com>
2023-12-04 03:03:15 +0300

    margin

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -12,11 +12,11 @@ const Navbar = async () => {
     <div className="border-b">
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center justify-between lg:justify-normal ">
-          <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-            <p className="font-bold text-xl">Home</p>
+          <Link href="/" className="ml-1 flex lg:ml-0 gap-x-2">
+            <p className="font-bold text-base lg:text-xl">Home</p>
           </Link>
-          <Link href="/store" className="flex lg:ml-2 gap-x-2">
-            <p className=" font-bold text-xl">Stores</p>
+          <Link href="/store" className="flex lg:ml-2 lg:mr-2 gap-x-2">
+            <p className=" font-bold text-base lg:text-xl">Stores</p>
           </Link>
           <MainNav data={categories} />
           <NavbarActions />
