3f49259e6704a604628f1f1c34cfdb376f31e268
Wafulah <wafulahvictor@gmail.com>
2024-05-06 00:58:05 +0300

    sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -6,7 +6,7 @@ import * as z from "zod";
 import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-
+import useUserStore from "@/lib/store";
 
 import Link from "next/link";
 import Image from "next/image";
@@ -42,20 +42,24 @@ export const Sidebar = () => {
   const pathname = usePathname();
   const [loading, setLoading] = useState(false);
 
-  
+  const userDetails = useUserStore((state) => state.items);
+  const userData = useUserStore();
+
+  const removeAll = useUserStore((state) => state.clearUserData); // Get the clearUserData function from the store
+
   const handleLogout = () => {
     // Call the removeAll function to clear user data
-    console.log("kot")
+    removeAll();
     // Optionally, navigate to the logout page or perform any other logout-related actions
   };
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      first_name: "userDetails[0].firstName",
-      last_name: "userDetails[0].lastName",
-      email: "userDetails[0].email",
-      phone_number: "userDetails[0].phoneNumber",
+      first_name: userDetails[0].firstName,
+      last_name: userDetails[0].lastName,
+      email: userDetails[0].email,
+      phone_number: userDetails[0].phoneNumber,
     },
   });
 
@@ -76,7 +80,7 @@ export const Sidebar = () => {
           phone_number,
         }
       );
-      
+      userData.setUserData(response.data);
 
       toast.success("Details Succesfully updated");
 
