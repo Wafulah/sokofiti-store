87c8150aa191ae02d668262d1e1eb7aa6940ac8c
Wafulah <wafulahvictor@gmail.com>
2024-05-06 03:08:43 +0300

    link

--- components/offer.tsx
+++ components/offer.tsx
@@ -5,8 +5,6 @@ import Link from "next/link";
 import NextImage from "next/image";
 import "keen-slider/keen-slider.min.css";
 
-
-
 const Offer = () => {
   const [sliderRef] = useKeenSlider<HTMLDivElement>(
     {
@@ -14,32 +12,32 @@ const Offer = () => {
     },
     [
       (slider) => {
-        let timeout: ReturnType<typeof setTimeout>
-        let mouseOver = false
+        let timeout: ReturnType<typeof setTimeout>;
+        let mouseOver = false;
         function clearNextTimeout() {
-          clearTimeout(timeout)
+          clearTimeout(timeout);
         }
         function nextTimeout() {
-          clearTimeout(timeout)
-          if (mouseOver) return
+          clearTimeout(timeout);
+          if (mouseOver) return;
           timeout = setTimeout(() => {
-            slider.next()
-          }, 2000)
+            slider.next();
+          }, 2000);
         }
         slider.on("created", () => {
           slider.container.addEventListener("mouseover", () => {
-            mouseOver = true
-            clearNextTimeout()
-          })
+            mouseOver = true;
+            clearNextTimeout();
+          });
           slider.container.addEventListener("mouseout", () => {
-            mouseOver = false
-            nextTimeout()
-          })
-          nextTimeout()
-        })
-        slider.on("dragStarted", clearNextTimeout)
-        slider.on("animationEnded", nextTimeout)
-        slider.on("updated", nextTimeout)
+            mouseOver = false;
+            nextTimeout();
+          });
+          nextTimeout();
+        });
+        slider.on("dragStarted", clearNextTimeout);
+        slider.on("animationEnded", nextTimeout);
+        slider.on("updated", nextTimeout);
       },
     ]
   );
@@ -77,17 +75,19 @@ const Offer = () => {
       className=" keen-slider h-52 
       lg:h-64 lg:w-[40vw] w-3/4 flex items-center justify-center rounded-md"
     >
-      {links.map((link, index) => (
-        <Link key={index} href={link.link} className="keen-slider__slide   h-52 
-          lg:h-64 w-full relative rounded-md aspect-square inset-0" >
-         
-            <NextImage
-              src={link.image}
-              fill
-              alt={link.name}
-              className="object-cover object-center"
-            />
-          
+      {links.map((link) => (
+        <Link
+          key={link.id}
+          href={link.link}
+          className="keen-slider__slide  h-52 
+          lg:h-64 w-full relative rounded-md aspect-square inset-0"
+        >
+          <NextImage
+            src={link.image}
+            fill
+            alt={link.name}
+            className="object-cover object-center"
+          />
         </Link>
       ))}
     </div>
