09cd55af24251eda199e6babbed6c05309b2b9ca
Wafulah <wafulahvictor@gmail.com>
2024-05-07 19:37:00 +0300

    images

--- components/cart-item.tsx
+++ components/cart-item.tsx
@@ -31,7 +31,7 @@ const CartItem: React.FC<CartItemProps> = ({ id, quantity }) => {
   }, [id]);
   const itemPrice = data?.price as string;
   const price = parseInt(itemPrice) * quantity;
-  console.log(data);
+
   return (
     <li className="flex py-6 border-b">
       <div className="relative h-24 w-24 rounded-md overflow-hidden sm:h-48 sm:w-48">
