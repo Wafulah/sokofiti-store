9c951d3648728f531263ce63c2a613860542a6c4
Wafulah <wafulahvictor@gmail.com>
2024-06-12 01:13:18 +0300

    title

--- app/(routes)/loading.tsx
+++ app/(routes)/loading.tsx
@@ -5,6 +5,12 @@ const Loading = () => {
   return (
     <Container>
       <div className="w-full h-full p-8">
+        <div className="mt-2 h-[8px] w-[60vw] md:w-[30vw] rounded-full">
+          <Skeleton className="aspect-square rounded-full w-full h-full bg-[rgba(255,0,0,0.54)]" />
+        </div>
+        <div className="mt-1 h-[6px] w-[30vw] md:w-[15vw] rounded-full">
+          <Skeleton className="aspect-square rounded-full w-full h-full bg-[rgba(255,0,0,0.54)]" />
+        </div>
         <div className="mt-3 grid grid-cols-4 gap-4">
           <Skeleton className="bg-[rgba(255,0,0,0.54)] aspect-square rounded-xl" />
           <Skeleton className="bg-[rgba(255,0,0,0.54)] aspect-square rounded-xl" />
