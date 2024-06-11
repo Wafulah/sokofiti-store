160c35c206e942aaa3d052612a323a15761325bd
Wafulah <wafulahvictor@gmail.com>
2024-06-12 01:10:15 +0300

    cols-span

--- app/(routes)/loading.tsx
+++ app/(routes)/loading.tsx
@@ -5,19 +5,28 @@ const Loading = () => {
   return (
     <Container>
       <div className="w-full h-full p-8">
-        <div className="w-full h-[200px] ">
-          <div className="h-[100px] w-max gap-4 grid grid-cols-4 ">
-            <Skeleton className="bg-[rgba(255,0,0,0.54)] aspect-square rounded-xl" />
-            <Skeleton className="bg-[rgba(255,0,0,0.54)] aspect-square rounded-xl" />
-            <Skeleton className="bg-[rgba(255,0,0,0.54)] aspect-square rounded-xl" />
-            <Skeleton className="bg-[rgba(255,0,0,0.54)] aspect-square rounded-xl" />
-          </div>
-        </div>
         <div className="mt-3 grid grid-cols-4 gap-4">
           <Skeleton className="bg-[rgba(255,0,0,0.54)] aspect-square rounded-xl" />
           <Skeleton className="bg-[rgba(255,0,0,0.54)] aspect-square rounded-xl" />
           <Skeleton className="bg-[rgba(255,0,0,0.54)] aspect-square rounded-xl" />
           <Skeleton className="bg-[rgba(255,0,0,0.54)] aspect-square rounded-xl" />
+        </div>
+        <div className="mt-3 grid grid-cols-1 lg:grid-cols-3 gap-4">
+          <div className="col-span-1">
+            <Skeleton className="bg-[rgba(255,0,0,0.54)] aspect-square rounded-xl" />
+          </div>
+          <div className="col-span-1 lg:hidden">
+            <Skeleton className="bg-[rgba(255,0,0,0.54)] aspect-square rounded-xl" />
+          </div>
+          <div className="col-span-1 lg:hidden">
+            <Skeleton className="bg-[rgba(255,0,0,0.54)] aspect-square rounded-xl" />
+          </div>
+          <div className="hidden lg:block col-span-1">
+            <Skeleton className="bg-[rgba(255,0,0,0.54)] aspect-square rounded-xl" />
+          </div>
+          <div className="hidden lg:block col-span-1">
+            <Skeleton className="bg-[rgba(255,0,0,0.54)] aspect-square rounded-xl" />
+          </div>
         </div>
 
         <div className="mt-2 h-[8px] w-[60vw] md:w-[30vw] rounded-full">
