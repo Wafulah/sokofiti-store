96e2d74517e233b79936617fe44da06706f96c02
Wafulah <wafulahvictor@gmail.com>
2024-06-12 00:59:01 +0300

    card

--- app/(routes)/loading.tsx
+++ app/(routes)/loading.tsx
@@ -7,23 +7,26 @@ const Loading = () => {
       <div className="w-full h-full p-8">
         <div className="w-full h-[200px] ">
           <div className="h-[100px] w-max gap-4 grid grid-cols-4 ">
-            <Skeleton className="aspect-square rounded-xl" />
-            <Skeleton className="aspect-square rounded-xl" />
-            <Skeleton className="aspect-square rounded-xl" />
-            <Skeleton className="aspect-square rounded-xl" />
+            <Skeleton className="bg-[rgba(255,0,0,0.54)] aspect-square rounded-xl" />
+            <Skeleton className="bg-[rgba(255,0,0,0.54)] aspect-square rounded-xl" />
+            <Skeleton className="bg-[rgba(255,0,0,0.54)] aspect-square rounded-xl" />
+            <Skeleton className="bg-[rgba(255,0,0,0.54)] aspect-square rounded-xl" />
           </div>
         </div>
-        <div className="mt-8 grid grid-cols-4 gap-4">
+        <div className="mt-3 grid grid-cols-4 gap-4">
           <Skeleton className="bg-[rgba(255,0,0,0.54)] aspect-square rounded-xl" />
           <Skeleton className="bg-[rgba(255,0,0,0.54)] aspect-square rounded-xl" />
           <Skeleton className="bg-[rgba(255,0,0,0.54)] aspect-square rounded-xl" />
           <Skeleton className="bg-[rgba(255,0,0,0.54)] aspect-square rounded-xl" />
         </div>
-        
-        <div className="h-[10px] w-[60vw] md:w-[30vw] rounded-full">
+
+        <div className="mt-2 h-[8px] w-[60vw] md:w-[30vw] rounded-full">
           <Skeleton className="aspect-square rounded-full w-full h-full bg-[rgba(255,0,0,0.54)]" />
         </div>
-        <div className="mt-8 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <div className="mt-1 h-[6px] w-[30vw] md:w-[15vw] rounded-full">
+          <Skeleton className="aspect-square rounded-full w-full h-full bg-[rgba(255,0,0,0.54)]" />
+        </div>
+        <div className="mt-4 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-4">
           <Skeleton className="bg-[rgba(255,0,0,0.54)] aspect-square rounded-xl" />
           <Skeleton className="bg-[rgba(255,0,0,0.54)] aspect-square rounded-xl" />
           <Skeleton className="bg-[rgba(255,0,0,0.54)] aspect-square rounded-xl" />
