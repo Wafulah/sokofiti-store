d590e42572fe75da5205c304345868e47de40e6f
Wafulah <wafulahvictor@gmail.com>
2024-06-12 00:34:29 +0300

    skeleton

--- app/(routes)/loading.tsx
+++ app/(routes)/loading.tsx
@@ -13,6 +13,9 @@ const Loading = () => {
             <Skeleton className="aspect-square rounded-xl" />
           </div>
         </div>
+        <div className="h-[20px] w-[80vw] md:w-[20vw] rounded-full">
+        <Skeleton className="aspect-square rounded-full w-full h-full" />
+        </div>
         <div className="mt-8 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-4">
           <Skeleton className="aspect-square rounded-xl" />
           <Skeleton className="aspect-square rounded-xl" />
